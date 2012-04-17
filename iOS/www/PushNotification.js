6f70e8fd766a3853c03beed85cfd49c9d8f62260
Chris Kihneman <ckihneman@gmail.com>
2012-04-17 11:24:35 -0700

    add cordova 1.6.0 support to PushNotification.js

--- iOS/www/PushNotification.js
+++ iOS/www/PushNotification.js
@@ -1,29 +1,25 @@
+(function(){
+	var cordovaRef = window.PhoneGap || window.Cordova || window.cordova;
 
-var PushNotification = function() {
+	var PushNotification = function() {};
 
-}
+	// call this to register for push notifications
+	PushNotification.prototype.register = function(success, fail, options) {
+	    cordovaRef.exec(success, fail, "PushNotification", "registerAPN", options);
+	};
 
-// call this to register for push notifications
-PushNotification.prototype.register = function(success, fail, options) {
-    Cordova.exec(success, fail, "PushNotification", "registerAPN", options);
-};
+	// call this to notify the plugin that the device is ready
+	PushNotification.prototype.startNotify = function(notificationCallback) {
+	    cordovaRef.exec(null, null, "PushNotification", "startNotify", []/* BUG - dies on null */);
+	};
 
-// call this to notify the plugin that the device is ready
-PushNotification.prototype.startNotify = function(notificationCallback) {
-    Cordova.exec(null, null, "PushNotification", "startNotify", []/* BUG - dies on null */);
-};
+	// use this to log from JS to the Xcode console - useful!
+	PushNotification.prototype.log = function(message) {
+	    cordovaRef.exec(null, null, "PushNotification", "log", [{"msg":message,}]);
+	};
 
-// use this to log from JS to the Xcode console - useful!
-PushNotification.prototype.log = function(message) {
-    Cordova.exec(null, null, "PushNotification", "log", [{"msg":message,}]);
-};
-
-
-Cordova.addConstructor(function() 
-{
-	if(!window.plugins)
-	{
-		window.plugins = {};
-	}
-	window.plugins.pushNotification = new PushNotification();
-});
+	cordovaRef.addConstructor(function() {
+		if(!window.plugins) window.plugins = {};
+		window.plugins.pushNotification = new PushNotification();
+	});
+})();
